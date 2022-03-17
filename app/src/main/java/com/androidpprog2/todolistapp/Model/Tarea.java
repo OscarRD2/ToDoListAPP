e1c334439e771dbcc6caab031d59a3bf325a2d6d
Oscar Romero Doblas <oscar.romero@students.salle.url.edu>
2022-03-17 18:22:45 +0100

    Class Task (DONE)

--- app/src/main/java/com/androidpprog2/todolistapp/Model/Tarea.java
+++ app/src/main/java/com/androidpprog2/todolistapp/Model/Tarea.java
@@ -1,5 +1,31 @@
 package com.androidpprog2.todolistapp.Model;
 
 public class Tarea {
-    
+
+    private String titulo;
+    private int id, status;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
